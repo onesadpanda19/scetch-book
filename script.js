f52a6f21fccff9e583aece0818e9b1f727cc893b
Andrew Nelson <nelsand19@gmail.com>
2023-07-24 18:55:42 -0600

    create scetchbook file, start basic loop

---
+++ script.js
@@ -0,0 +1,6 @@
+for(i=0; i<16; i++) {
+    let squareContainer = document.createElement('div');
+    squareContainer.className = "square";
+    document.getElementById('squareContainer').appendChild(squareContainer);
+
+}
