722fbcd9bf0a4820aaf4ab84cdc7cde9ba040b87
Andrew Nelson <nelsand19@gmail.com>
2023-07-27 19:06:23 -0600

    add loop to make squares, fix css to fit squares into the container

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-for(i=0; i<16; i++) {
-    let squareContainer = document.createElement('div');
-    squareContainer.className = "square";
-    document.getElementById('squareContainer').appendChild(squareContainer);
+let squareContainer;
 
+
+function genDivs(v) {
+    let e = document.getElementById("squareContainer") ;
+    for(let i = 0; i < v; i++) {
+      let row = document.createElement("div");
+      row.className ="row";
+      for(let x = 1; x <= v; x++){
+          let cell = document.createElement("div")
+          cell.className = "square";
+          cell.innerText = (i * v) + x;
+          row.appendChild(cell)
+      }  
+      e.appendChild(row) 
+    }
+    document.getElementById('square').innerText = e.innerHTML;
 }
+genDivs(6)
