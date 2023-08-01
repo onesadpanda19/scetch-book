71735cbd2e8a7545aab612cca1bc3689db9548a4
Andrew Nelson <nelsand19@gmail.com>
2023-07-31 19:00:40 -0600

    add cell width and height

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 let squareContainer;
 
 
-function genDivs(v) {
-    let e = document.getElementById("squareContainer") ;
-    for(let i = 0; i < v; i++) {
+function genDivs(column) {
+    column = column || 16;
+    let divWidth = 20/column + "rem";
+    let divHight = 20/column + "rem";
+    let container = document.getElementById("squareContainer");
+    for(let i = 0; i < column; i++) {
       let row = document.createElement("div");
       row.className ="row";
-      for(let x = 1; x <= v; x++){
+      for(let x = 1; x <= column; x++){
           let cell = document.createElement("div")
           cell.className = "square";
-          cell.innerText = (i * v) + x;
+          cell.innerText = (i * column) + x;
           row.appendChild(cell)
+          const widthHight= document.createAttribute('style');
+          widthHight.value = `width: ${divWidth}; height : ${divHight}` 
+          cell.setAttributeNode(widthHight);
+         
       }  
-      e.appendChild(row) 
+      container.appendChild(row) 
     }
-    document.getElementById('square').innerText = e.innerHTML;
+    document.getElementById('square').innerText = container.innerHTML;
 }
-genDivs(6)
+genDivs(24)
